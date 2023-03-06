8f63576c7f8546b9be29646886525ca54a1c6f5c
Davi Ferreira <davi.ferreira2008@hotmail.com>
2023-03-06 11:31:47 -0300

    teste

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ export const Header = () =>{
     return(
         <StyledHeader>
             <img src={LogoDivideAi} alt="LogoDivideAi" />
-            <p>Oi, esse é o Divide Aí</p>
+            <p>Oi, esse é o Divide Aí!</p>
         </StyledHeader>
     )
 }
