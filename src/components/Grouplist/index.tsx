d4e43a112d7708969905ee882bf2cea14ed9e430
Maksuel Nascimento <mkdias_nascimento@hotmail.com>
2023-03-09 14:59:35 -0300

    feat: adiciona renderização condicional dos cards

--- src/components/Grouplist/index.tsx
+++ src/components/Grouplist/index.tsx
@@ -1,13 +1,15 @@
+import { useContext } from "react";
+import { ContextUser } from "../../providers/ContextUser";
 import { GroupCard } from "./GroupCard";
 import { StyledGroupList } from "./styles";
 
 export const GroupList = () => {
+  const { client } = useContext(ContextUser);
   return (
     <StyledGroupList>
       <GroupCard example="banana" />
       <GroupCard example="banana" />
-      <GroupCard example="banana" />
-      <GroupCard example="banana" />
+      {client ? <GroupCard key={crypto.randomUUID()} example="paçoca" /> : null}
     </StyledGroupList>
   );
 };
