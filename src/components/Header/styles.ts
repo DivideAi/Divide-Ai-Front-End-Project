9a946570838716b6aa2ea6867b771fed37a7ba9e
Maksuel Nascimento <mkdias_nascimento@hotmail.com>
2023-03-09 16:27:54 -0300

    feat: cria navbar e adiciona título condicional da header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
 
 export const StyledHeader = styled.header`
-display: flex;
-align-items: center;
-justify-content: center;
-margin-top: 2rem;
-
-`
+  display: flex;
+  align-items: center;
+  flex-flow: column nowrap;
+  gap: 40px;
+  justify-content: center;
+  margin-top: 2rem;
+`;
