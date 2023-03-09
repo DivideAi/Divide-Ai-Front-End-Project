3fc14f834bc73620d45a31b50e3e2aa17f8909cb
Maksuel Nascimento <114680477+mk-nascimento@users.noreply.github.com>
2023-03-09 16:46:12 -0300

    Revert "feat/navbar"

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { GlobalStyles } from './styles/global'
-import { Router } from './routes';
+import { Header } from './components/Header'
 
 function App() {
   return (
     <>
     <GlobalStyles/>
-    <Router/>
+    <Header/>
+    
     </>
-  
   );
 }
 
