80d97e13d2ee7c2df5d9f6d99ad705668e3b89fd
Herli Khoury <92793769+HerliKhoury@users.noreply.github.com>
2023-03-09 17:20:37 -0300

    feat: login schema

--- src/components/Forms/LoginForm/index.tsx
+++ src/components/Forms/LoginForm/index.tsx
@@ -1,12 +1,44 @@
 import { StyledLoginForm } from "./styles";
 import { GeneralInput } from "../../GeneralInput";
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+
+interface iLoginForm{
+    email: string;
+    password: string;
+}
 
 export const LoginForm = () => {
 
+    const loginSchema = yup.object().shape({
+        email: yup.string().required('E-mail obrigatório!').email('E-mail inválido!'),
+        password: yup.string().required('Senha obrigatória'),
+    });
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<iLoginForm>({
+        resolver: yupResolver(loginSchema),
+    });
+
     return(
-        <StyledLoginForm>
-            {/* <GeneralInput placeholder="se input" {} type={} error={}/>
-            <GeneralInput placeholder="se input" {} type={} error={}/> */}
+        <StyledLoginForm onSubmit={handleSubmit(/* chamar aqui a log user */)}>
+            <GeneralInput 
+            placeholder="Email" 
+            register={register("email")} 
+            type={"email"} 
+            error={errors.email}
+            />
+            <GeneralInput 
+            placeholder="Senha" 
+            register={register("password")} 
+            type={"password"} 
+            error={errors.password}
+            />
         </StyledLoginForm>
     )
 }
