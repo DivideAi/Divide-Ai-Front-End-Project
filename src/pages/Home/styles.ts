136cb8a0f255806a923226be7c57675d32e515e9
Herli Khoury <92793769+HerliKhoury@users.noreply.github.com>
2023-03-10 15:55:21 -0300

    feat: Link style

---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+export const StyledHomePage = styled.main`
+    display: flex;
+    flex-direction: column;
+    padding-top: 50px;
+
+    img{
+        width: 220px;
+        height: 145.26px;
+        margin: 0 auto;
+    }
+
+    p{
+        display: block;
+        width: 240px;
+        margin: 0 auto;
+        padding-top: 60px;
+    }
+    
+    .Link_style{
+        width: 320px;
+        height: 45px;
+        color: #3295AD;
+        background-color: #FFFFFF;
+        border: none;
+        border-radius: 10px;
+    }
+
+`
