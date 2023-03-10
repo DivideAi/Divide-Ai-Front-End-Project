acfc0a68bd08c15fac26b5d052f0461d86e2ade9
Herli Khoury <92793769+HerliKhoury@users.noreply.github.com>
2023-03-09 22:08:51 -0300

    feat: LogUser function

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,8 +1,18 @@
-import { Header } from "../../components/Header";
-import { LoginForm } from "../../components/Forms/LoginForm"
-
+import { useEffect } from 'react'
+import { Header } from '../../components/Header'
+import { LoginForm } from '../../components/Forms/LoginForm'
+import { useNavigate } from 'react-router-dom'
+import { isLogged } from '../../scripts/localStorage'
 
 export const Login = () =>{
+    const navigation = useNavigate()
+
+    useEffect(() => {
+        if (isLogged()) {
+         navigation(-1);
+        }
+    }, [])
+
     return(
         <>
             <Header/>
