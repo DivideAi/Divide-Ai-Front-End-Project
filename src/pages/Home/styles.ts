e776893f3ee7c4f2d23b5b0056c8d254aaeb534d
Herli Khoury <92793769+HerliKhoury@users.noreply.github.com>
2023-03-11 09:39:45 -0300

    feat: Home page style

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -5,6 +5,14 @@ export const StyledHomePage = styled.main`
     flex-direction: column;
     padding-top: 50px;
 
+    @media(min-width: 640px){
+        border: 2px solid #3295AD;
+        border-radius: 10px;
+        padding: 3rem;
+        width: 60%;
+        margin: 2rem auto;
+    }
+
     img{
         width: 220px;
         height: 145.26px;
@@ -16,15 +24,23 @@ export const StyledHomePage = styled.main`
         width: 240px;
         margin: 0 auto;
         padding-top: 60px;
+        margin-bottom: 70px;
     }
     
     .Link_style{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        justify-self: center;
+        margin: 20px auto;
         width: 320px;
         height: 45px;
         color: #3295AD;
         background-color: #FFFFFF;
         border: none;
         border-radius: 10px;
+        font-size: 1.125rem;
+        font-weight: 700;
     }
 
 `
