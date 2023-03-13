de38abd5f64f4cfd652103a7e278e7355a5ccb3f
Davi Ferreira <davi.ferreira2008@hotmail.com>
2023-03-13 15:18:01 -0300

    fix: broken styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,16 @@
 import LogoDivideAi from '../../assets/Logo Header.png'
 import { StyledHeader } from './styles'
+import LogoFull from '../../assets/logoDivideAi.svg'
 
 interface iHeaderProps {
+  home?: boolean;
   description?: string;
   title?: string;
 }
 export const Header = (props: iHeaderProps) => {
   return (
     <StyledHeader>
-      <img className='logo-image' src={LogoDivideAi} alt='LogoDivideAi' />
+      <img className='logo-image' src={props.home? LogoFull : LogoDivideAi} alt='LogoDivideAi' />
       {props.title ? (
         <h1 className='header-title Title1'>{props.title}</h1>
       ) : null}
