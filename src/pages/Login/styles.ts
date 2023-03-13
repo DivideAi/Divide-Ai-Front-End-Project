a2a922cede3638cfed8f8915f4c2e848bfcbb9fb
Herli Khoury <92793769+HerliKhoury@users.noreply.github.com>
2023-03-13 13:40:40 -0300

    feat: clean global style

--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,20 +1,11 @@
 import styled from 'styled-components'
 import { Main } from '../../styles/main'
 
+
+
 export const StyledLoginPage = styled(Main)`
     dislplay: flex;
-    h1{
-        text-align-last: center;
-        padding-bottom: 12px;
-    }
-
-    p{
-        display:block;
-        width: 268px;
-        text-align-last: center;
-        margin: 0 auto;
-    }
-
+    
     @media(min-width: 640px){
         border: 2px solid #3295AD;
         border-radius: 10px;
--- src/pages/Register/styles.ts
+++ src/pages/Register/styles.ts
@@ -1,18 +1,13 @@
 import styled from 'styled-components'
 import { Main } from '../../styles/main'
 
+
+
 export const StyledRegisterPage = styled(Main)`
     dislplay: flex;
-    h1{
-        text-align-last: center;
-        padding-bottom: 12px;
-    }
-
+    
     p{
-        display:block;
         width: 320px;
-        text-align-last: center;
-        margin: 0 auto;
     }
 
     @media(min-width: 640px){
